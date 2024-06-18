44cd131518c3159d61535474e03c96d7e617a48f
Gina Sarfaty <gsarfaty@usaid.gov>
2024-06-18 16:43:34 -0400

    Session 1 Summary Exercise

---
+++ Scripts/Session_1/02_session1_summary.R
@@ -0,0 +1,35 @@
+# AUTHOR:   G. Sarfaty & D. Collison | USAID
+# PURPOSE:  Namibia R Training | Session 1 Summary
+# LICENSE:  MIT
+# DATE:     2024-06-18
+# UPDATED: 
+
+# DEPENDENCIES ------------------------------------------------------------
+
+#EXERCISE Q1: load tidyverse and gophr packages
+
+
+
+# IMPORT DATA -------------------------------------------------------------
+
+#EXERCISE Q2: You will be using list.files. Write a line of code below to pull 
+#up the help for list files
+
+
+
+#EXERCISE Q3: Use list.files to create a new object called msd_path that 
+# points to the PSNU_IM dataset in the Data folder.
+
+
+#EXERCISE Q4: Use read_psd from the gophr package to read in the PSNUxIM data,
+# and assign it as an object called df_msd
+
+
+#EXERCISE Q5: Access the indicator column of the df_msd data frame
+
+
+# MUNGE -------------------------------------------------------------------
+
+#NO EXERCISE YET: We are going to start inspecting, isolating, and manipulating
+# data next!
+'
