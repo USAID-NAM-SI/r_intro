23e4e2e0a58debf4fded9ecbfde54da147260c77
Gina Sarfaty <gsarfaty@usaid.gov>
2024-06-19 16:11:20 -0400

    answers added

--- Scripts/Session_1/02_session1_summary.R
+++ Scripts/Session_1/02_session1_summary.R
@@ -7,6 +7,8 @@
 # DEPENDENCIES ------------------------------------------------------------
 
 #EXERCISE Q1: load tidyverse and gophr packages
+library("tidyverse")
+library("gophr")
 
 
 
@@ -14,19 +16,19 @@
 
 #EXERCISE Q2: You will be using list.files. Write a line of code below to pull 
 #up the help for list files
-
+?list.files
 
 
 #EXERCISE Q3: Use list.files to create a new object called msd_path that 
 # points to the PSNU_IM dataset in the Data folder.
-
+msd_path<-list.files("Data",pattern="PSNU",full.names = T)
 
 #EXERCISE Q4: Use read_psd from the gophr package to read in the PSNUxIM data,
 # and assign it as an object called df_msd
-
+df_msd<-read_psd(msd_path)
 
 #EXERCISE Q5: Access the indicator column of the df_msd data frame
-
+df_msd$indicator
 
 # MUNGE -------------------------------------------------------------------
 
